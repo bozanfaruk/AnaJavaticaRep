4d3cde4a76c0cfe49ac042f35ed80e14f9ae8f0f
bozanfaruk <bozanfaruk@gmail.com>
2015-09-12 16:11:10 +0300

    Temp file commit.

---
+++ AnaJavatica/src/anajavatica/file/TempFileMain.java
@@ -0,0 +1,34 @@
+package anajavatica.file;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class TempFileMain {
+
+	private File createTempFile() throws IOException {
+		File tempFile = File.createTempFile("AnaJavaticaTempFile", ".anajavatica");
+		BufferedWriter bw = new BufferedWriter(new FileWriter(tempFile));
+		bw.write("AnaJavatica Temp File");
+		bw.flush();
+		bw.close();
+		return tempFile;
+	}
+
+	public static void main(String[] args) {
+
+		TempFileMain instance = new TempFileMain();
+		File tempFile = null;
+		try {
+			tempFile = instance.createTempFile();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		if (tempFile != null)
+			System.out.println(tempFile.getAbsolutePath());
+
+	}
+
+}
