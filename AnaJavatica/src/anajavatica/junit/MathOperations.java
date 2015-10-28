34fed1b5642133c35c91c7ce0dc4f07a93250dd6
bozanfaruk <bozanfaruk@gmail.com>
2015-10-28 23:02:38 +0200

    JUnit commit

---
+++ AnaJavatica/src/anajavatica/junit/MathOperations.java
@@ -0,0 +1,21 @@
+package anajavatica.junit;
+
+public class MathOperations {
+
+	public int sum(int val1, int val2) {
+		return val1 + val2;
+	}
+
+	public int diff(int val1, int val2) {
+		return val1 - val2;
+	}
+
+	public int mul(int val1, int val2) {
+		return val1 * val2;
+	}
+
+	public int div(int val1, int val2) {
+		return val1 / val2;
+	}
+
+}
---
+++ AnaJavatica/src/anajavatica/junit/MathOperationsTest.java
@@ -0,0 +1,56 @@
+package anajavatica.junit;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class MathOperationsTest {
+
+	private static MathOperations mathOperations = null;
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		mathOperations = new MathOperations();
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		mathOperations = null;
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testSum() {
+		int result = mathOperations.sum(1, 2);
+		Assert.assertTrue(result == 3);
+	}
+
+	@Test
+	public void testDiff() {
+		int result = mathOperations.diff(5, 1);
+		Assert.assertTrue(result == 4);
+	}
+
+	@Test
+	public void testMul() {
+		int result = mathOperations.mul(5, 3);
+		Assert.assertEquals(15, result);
+	}
+
+	@Test
+	public void testDiv() {
+		int result = mathOperations.div(5, 1);
+		Assert.assertEquals(4, result);
+	}
+
+}
