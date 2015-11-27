b18640e0c4b0d037805eb13b84cc655f43470c88
bozanfaruk <bozanfaruk@gmail.com>
2015-11-27 13:29:14 +0200

    TempFileMain TODO remove.

--- AnaJavatica/src/anajavatica/file/TempFileMain.java
+++ AnaJavatica/src/anajavatica/file/TempFileMain.java
@@ -23,7 +23,6 @@ public class TempFileMain {
 		try {
 			tempFile = instance.createTempFile();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		if (tempFile != null)
