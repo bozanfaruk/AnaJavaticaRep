89cb0016572297915180d05ed80fef5a40d0582a
bozanfaruk <bozanfaruk@gmail.com>
2015-12-11 09:34:26 +0200

    System.in commit.

---
+++ AnaJavatica/src/anajavatica/system/SystemMain.java
@@ -0,0 +1,23 @@
+package anajavatica.system;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class SystemMain {
+
+	public static void main(String[] args) throws IOException {
+
+		Scanner scanner = new Scanner(System.in);
+		System.out.println(scanner.nextLine());
+		scanner.close();
+
+		System.setIn(new FileInputStream(new File("scanner.txt")));
+		scanner = new Scanner(System.in);
+		System.out.println(scanner.nextLine());
+		scanner.close();
+
+	}
+
+}
